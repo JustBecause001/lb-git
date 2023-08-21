508d3451f44c108a4ec295968cadd457ea7b972b
JustBecause001 <135592771+JustBecause001@users.noreply.github.com>
2023-08-21 10:15:41 +0800

    Update lb.java

--- lb.java
+++ lb.java
@@ -2,5 +2,6 @@
 		System.out.println("myfirst git file");
 		System.out.println("lb master first file");
 		System.out.println("git_02 firest file");
-		System.out.println("github file")
+		System.out.println("github file");
+		System.out.println("github file 2");
 	}
